17a26942e2ba4b8d3c2e85a3e3977512ade0b455
Tim Kye <tim@kye.dev>
2021-07-28 20:09:47 -0700

    more wip

--- src/deps.ts
+++ src/deps.ts
@@ -3,6 +3,6 @@ export { json, serve, validateRequest } from 'https://deno.land/x/sift@0.3.3/mod
 export * from 'https://raw.githubusercontent.com/discordeno/discordeno/main/src/interactions/mod.ts'
 export * from 'https://raw.githubusercontent.com/discordeno/discordeno/main/mod.ts'
 export { decode } from 'https://deno.land/std/encoding/base64url.ts'
-export times from 'https://deno.land/x/lodash@4.17.15-es/times.js'
-export sum from 'https://deno.land/x/lodash@4.17.15-es/sum.js'
+export { default as times } from 'https://deno.land/x/lodash@4.17.15-es/times.js'
+export { default as sum } from 'https://deno.land/x/lodash@4.17.15-es/sum.js'
 export { default as deepmerge } from 'https://cdn.skypack.dev/deepmerge'
